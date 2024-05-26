c33518793a2a17d6f0886e8c59f19878164fb9ae
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2024-05-26 13:45:15 +0200

    feat: lentilles section casi feta

--- src/components/LentillasGrid.jsx
+++ src/components/LentillasGrid.jsx
@@ -1,15 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
+import lentillaGraduadaUno from "../img/lentilla-graduada.jpg"
+import lentillaGraduadaDos from "../img/lentilla-grduada-2.jpeg"
+import lentillaGraduadaTres from "../img/lentilla-graduada-3.webp"
+import lentillaGraduadaCuatro from "../img/lentillas-progresivas.png"
+import lentillaColor from "../img/lentilla-color.jpg"
+import lentillaCosmetica from "../img/lentilla-cosmetica.jpg"
 
 export const LentillasGrid = () => {
   return (
     <div className='w-full flex justify-center mt-20'>
-        <div class="grid lg:grid-cols-3 grid-cols-1 gap-8 w-full px-20 text-center">
-            <div className="border-2 h-96">x</div>
-            <div className="border-2 h-96">y</div>
-            <div className="border-2 h-96">z</div>
-            <div className="border-2 h-96">w</div>
-            <div className="border-2 h-96">y</div>
-            <div className="border-2 h-96">x</div>
+        <div class="grid lg:grid-cols-3 grid-cols-1 gap-8 w-full px-40 text-center">
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaGraduadaUno} alt='lentilla' />
+              <span>Lentilles Diaries</span>
+            </div>
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaGraduadaTres} alt='lentilla' />
+              <span>Lentilles 15 díes</span>
+            </div>
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaGraduadaDos} alt='lentilla' />
+              <span>Lentilles mensuals</span>
+            </div>
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaGraduadaCuatro} alt='lentilla' />
+              <span>Lentillas d&apos;us prolongat</span>
+            </div>
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaColor} alt='lentilla' />
+              <span>Lentillas de color</span>
+            </div>
+            <div className="h-96">
+              <Image className='w-full h-full object-cover rounded-md hover:scale-95' src={lentillaCosmetica} alt='lentilla' />
+              <span>Lentillas cosmètiques</span>
+            </div>
         </div>
     </div>
   )
--- src/components/LentillasHeader.jsx
+++ src/components/LentillasHeader.jsx
@@ -1,13 +1,93 @@
-import React from 'react'
+import React from "react";
+import mujerHorizontal from "../img/lentillas-solas.webp";
+import mujerVertical from "../img/mujer-v-left.webp";
+import whatsapp from "../img/whatsapp.svg";
+import Image from "next/image";
+import "../app/globals.css";
 
 export const LentillasHeader = () => {
   return (
-    <div className='flex justify-center w-full h-[800px]'>
-        <div className='lg:w-2/6 w-full border-4 border-red-400 h-full bg-yellow-200'></div>
-        <div className='w-4/6 lg:block hidden border-4 border-red-400 h-full'>
-            <div className='h-4/6 border-2 border-blue-300 w-full bg-yellow-200'>x</div>
-            <div className='h-2/6 border-2 border-blue-300 w-full'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita quibusdam modi iste optio quod laboriosam, explicabo non similique harum a quis animi officia quaerat quia eaque dicta doloribus dolores incidunt nobis. Expedita sed, obcaecati perferendis eius quidem sapiente facere optio voluptatem illum recusandae consequatur veritatis explicabo quo ut praesentium saepe et corrupti. Sed qui beatae neque accusantium, ullam voluptatibus. Libero inventore nostrum voluptatem tempora possimus consequatur, placeat nihil iure maiores debitis amet perspiciatis magni, incidunt perferendis error, voluptatum laudantium aspernatur!</div>
+    <div className="flex justify-center w-full h-[800px] bg-slate-200">
+      <div className="lg:w-2/6 w-full flex flex-col">
+        <Image
+          className="w-full h-full object-cover object-left customMask"
+          src={mujerVertical}
+          alt="ojos-azules"
+        />
+        <div className="w-full h-52 bg-[#cbd9c7] lg:hidden block">
+          <div className="w-full flex flex-col justify-center px-3 pt-5 text-balance">
+            <h2 className="text-xl text-center">
+              Descobreix la nostra gran oferta en lents de contacte!
+            </h2>
+            <span className="w-full flex text-center justify-center">
+              A Òptica Guinart, t&apos;oferim lents de contacte de la millor qualitat
+              a preus incomparables.
+            </span>
+
+            <div className="flex flex-col lg:flex-row justify-center items-center my-3 px-2">
+              <a
+                href="https://api.whatsapp.com/send?phone=+34682502960&text=Hola!%20Quería%20informaci%C3%B3n%20sobre:"
+                target="_blank"
+                className="inline-flex items-center justify-center p-2 lg:bg-[#A2DED0] bg-green-400 rounded-md hover:bg-green-500 transition duration-300"
+              >
+                <Image
+                  className="hover:invert-50 transition duration-300"
+                  src={whatsapp}
+                  alt="Whatsapp-icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+              <p className="text-center text-black lg:ml-3">
+                Contacta amb nosaltres!
+              </p>
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="w-4/6 lg:block hidden h-full">
+        <div className="h-4/6 w-full">
+          <Image
+            className="w-full h-full object-cover customMaskTwo"
+            src={mujerHorizontal}
+            alt="ojos-azules"
+          />
+        </div>
+        <div className="h-2/6 w-full flex flex-col justify-center px-3 pt-5 text-balance">
+          <h2 className="text-xl">
+            Descobreix la nostra gran oferta en lents de contacte!
+          </h2>
+          <span className="w-full flex justify-center">
+            A Òptica Guinart, estem emocionats d&apos;oferir-te una àmplia gamma
+            de lents de contacte de la millor qualitat a preus incomparables.
+            <br />
+            Busques lents de contacte graduades per corregir la teva visió?
+            <br />O potser prefereixes lentilles progressives per a una visió
+            clara a totes les distàncies?
+            <br />
+            Sigui quina sigui la teva necessitat, tenim el que busques!!
+          </span>
+
+          <div className="flex flex-col lg:flex-row justify-center items-center my-3 px-2">
+            <a
+              href="https://api.whatsapp.com/send?phone=+34682502960&text=Hola!%20Quería%20informaci%C3%B3n%20sobre:"
+              target="_blank"
+              className="inline-flex items-center justify-center p-2 bg-[#A2DED0] rounded-md hover:bg-green-500 transition duration-300"
+            >
+              <Image
+                className="hover:invert-50 transition duration-300"
+                src={whatsapp}
+                alt="Whatsapp-icon"
+                width={24}
+                height={24}
+              />
+            </a>
+            <p className="text-center text-black lg:ml-3">
+              ¡Contacta amb nosaltres!
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
