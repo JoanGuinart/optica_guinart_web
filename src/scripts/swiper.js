6071b1775caf461dfcb7ecb0d6d89c8b57b372f1
Joan Guinart <guinartcasasjoan@gmail.com>
2024-11-24 16:57:00 +0100

    fix: swiper parallaz and color

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,20 +1,31 @@
 import Swiper from "swiper";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Navigation, Pagination, Autoplay, Mousewheel, Parallax } from "swiper/modules";
 
 document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper(".mySwiper", {
     direction: "horizontal",
     loop: true,
-    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    modules: [Navigation, Pagination, Autoplay, Mousewheel, Parallax],
     autoplay: {
       delay: 3500,
       disableOnInteraction: false,
       pauseOnMouseEnter: false,
     },
+    freeMode: true,
+    mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+    },
+    parallax: {
+      enabled: true,
+    },
     loop: true,
-    speed: 2000,
+    speed: 1000,
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
